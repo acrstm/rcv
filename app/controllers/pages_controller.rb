bb3addd2adf98d1a94d0a79846e9413975af12cb
Alam Sena <alam@koarquitetos.com>
2023-05-17 15:42:50 +0200

    lecture

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
+
   def home
   end
+
+  def about
+  end
+
+  def contact
+    @members = ["claire", "toni", "sarah", "olivier"]
+    search = params[:member]
+    if search
+      @members = @members.select do |member|
+        member.start_with? search.downcase
+      end
+    end
+  end
+
 end
